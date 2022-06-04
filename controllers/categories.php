dec734f948b1c2ae40e10619ce89809003938de2
victor <alesana_sv@hotmail.com>
2022-06-04 14:47:16 -0500

    1.6.2

--- controllers/categories.php
+++ controllers/categories.php
@@ -1,17 +1,71 @@
 <?php
-    class Categories extends Controller {
+    class Categories extends SessionController {
 
+        private $user;
 
         function __construct(){
             parent::__construct(); 
             $this->loadModel('categories');
+            $this->user = $this->getUserSessionData();
         }
 
         function render(){
             $categories = new CategoriesModel();
             $this->view->render('categories/index', [
+                'user' => $this->user,
+                'categories' => $categories->getAllByUser($this->user->getId())
             ]);
         }
+        // peticiondeleteItem
+        function deleteItem(){
+            if (!$this->existPOST(['id'])) {
+                $this->redirect('categories', []); // TODO:
+                return;
+            }
+            $id = $this->getPost('id');
+
+            if ($this->user == NULL || empty($id)) {
+                $this->redirect('categories', []); // TODO: 
+            }
+
+            $category = new CategoriesModel();
+            $category->get($id);
+
+            if($category->delete($category->getId())){
+                $this->redirect('categories', []); //TODO:
+            }
+
+            $this->redirect('categories', []); // TODO: 
+        }
+        
+        // peticion
+        function updateItem(){
+            if (!$this->existPOST(['id','title', 'type'])) {
+                $this->redirect('categories', []); // TODO:
+                return;
+            }
+            $id = $this->getPost('id');
+            $title = $this->getPost('title');
+            $type = $this->getPost('type');
+
+            if ($this->user == NULL || empty($id) || empty($title) || empty($type) || empty($user_id) ) {
+                $this->redirect('categories', []); // TODO: 
+            }
+
+            $category = new CategoriesModel();
+            $category->get($id);
+            $category->setTitle($title);
+            $category->setType($type);
+            $category->setUser_id($this->user->getId());
+
+            if($category->update()){
+                $this->redirect('categories', []); //TODO:
+            }
+
+            $this->redirect('categories', []); // TODO: 
+        }
+
+
 
     }
 ?>
--- controllers/createCategory.php
+++ controllers/createCategory.php
@@ -1,16 +1,47 @@
 <?php
-    class CreateCategory extends Controller {
+    class CreateCategory extends SessionController {
 
+        private $user;
 
         function __construct(){
             parent::__construct(); 
             $this->loadModel('categories');
+            $this->user = $this->getUserSessionData();
         }
 
         function render(){
             $category = new CategoriesModel();
+            $categories = $category->getAllExpenseCategoriesByUser($this->user->getId());
+            $categories = $category->getAllExpenseCategoriesByUser($this->user->getId());
             $this->view->render('categories/create',[
+                'user' => $this->user,
+                'categories' => $categories,
             ]);
+        }
+
+        // peticion
+        function saveItem(){
+            if (!$this->existPOST(['title', 'type'])) {
+                $this->redirect('createCategory', []); // TODO:
+                return;
+            }
+            $title = $this->getPost('title');
+            $type = $this->getPost('type');
+
+            if ($this->user == NULL || empty($id) || empty($title) || empty($type) || empty($user_id) ) {
+                $this->redirect('createCategory', []); // TODO: 
+            }
+
+            $category = new CategoriesModel();
+            $category->setTitle($title);
+            $category->setType($type);
+            $category->setUser_id($this->user->getId());
+
+            if($category->save()){
+                $this->redirect('createCategory', []); //TODO:
+            }
+
+            $this->redirect('createCategory', []); // TODO: 
         }
 
     }
